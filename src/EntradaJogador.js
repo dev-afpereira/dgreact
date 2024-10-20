861270e7a42ec23a83d358dcf1040e1ee8c74df8
Dev-afpereira <pereira.afr@gmail.com>
2024-10-20 16:50:38 +0100

    Versao 1.1.2

--- src/EntradaJogador.js
+++ src/EntradaJogador.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './EntradaJogador.css';
 
 function EntradaJogador({ onAddPlayer }) {
   const [nome, setNome] = useState('');
@@ -7,32 +8,40 @@ function EntradaJogador({ onAddPlayer }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (nome.trim() !== '') {
-      onAddPlayer(nome, gameId);
+      if (gameId.trim() !== '') {
+        onAddPlayer(nome, gameId, 'join');
+      } else {
+        onAddPlayer(nome, null, 'create');
+      }
+    } else {
+      alert('Por favor, insira seu nome.');
     }
   };
 
   return (
-    <div className="card">
-      <h1 className="title">Jogo do Número</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-          placeholder="Seu nome"
-          className="input"
-        />
-        <input
-          type="text"
-          value={gameId}
-          onChange={(e) => setGameId(e.target.value)}
-          placeholder="ID do Jogo (opcional)"
-          className="input"
-        />
-        <button type="submit" className="button primary-button">
-          {gameId ? 'Juntar-se ao Jogo' : 'Criar Novo Jogo'}
-        </button>
-      </form>
+    <div className="entrada-container">
+      <div className="card">
+        <h1 className="title">Jogo do Número</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Seu nome"
+            className="input"
+          />
+          <input
+            type="text"
+            value={gameId}
+            onChange={(e) => setGameId(e.target.value)}
+            placeholder="ID do Jogo (opcional)"
+            className="input"
+          />
+          <button type="submit" className="button">
+            {gameId ? 'Juntar-se ao Jogo' : 'Criar Novo Jogo'}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
